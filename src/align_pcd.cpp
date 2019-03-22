0c55c9d6b710d6ba1535a0f2197798eec65a31a2
Fahira Afzal Maken <fahiraa@hotmail.com>
2019-03-22 14:08:02 +1100

    added momentum option

--- src/align_pcd.cpp
+++ src/align_pcd.cpp
@@ -1,3 +1,39 @@
+/*
+ Copyright (c) 2019
+ 
+ Fahira Afzal Maken and Lionel Ott,
+ The University of Sydney, Australia.
+ 
+ All rights reserved.
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+ 
+ 1. Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ 
+ 2. Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in
+ the documentation and/or other materials provided with the distribution.
+ 
+ 3. Neither the name of the copyright holder nor the names of its
+ contributors may be used to endorse or promote products derived from
+ this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ */
+
+
 #include <cmath>
 
 #include <boost/program_options.hpp>
@@ -9,10 +45,10 @@
 #include <pcl/filters/filter.h>
 #include <pcl/io/pcd_io.h>
 
-
-#include "fixed_sgd.h"
 #include "adadelta.h"
 #include "adam.h"
+#include "fixed_sgd.h"
+#include "momentum.h"
 #include "rmsprop.h"
 #include "sgdicp.h"
 #include "types.h"
@@ -153,6 +189,21 @@ int main(int argc, char * argv[])
                 )
         ));
     }
+    
+    
+    else if (config.get<std::string>("method") == "momentum")
+    {
+        sgd_icp.reset(new SGDICP(
+                                 std::unique_ptr<MomentumSgd>(
+                                                    new MomentumSgd(
+                                                                 initial_guess,
+                                                                 config.get<double>("momentum.step-size")
+                                                                     )
+                                                              )
+                                                     ));
+    }
+    
+    
     else if(config.get<std::string>("method") == "rmsprop")
     {
         sgd_icp.reset(new SGDICP(
@@ -169,7 +220,7 @@ int main(int argc, char * argv[])
     
     {
         std::cout << "Invalid optimizer specified, valid optiosn are: "
-                  << "adadelta, adam, fixed, rmsprop" << std::endl;
+                  << "adadelta, adam, fixed, momentum, and rmsprop" << std::endl;
         return 1;
     }
 
