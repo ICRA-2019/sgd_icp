8f0a33b745e8db573d0b0abd2349d7e06c97ca08
Lionel Ott <white.magic@gmx.de>
2019-04-29 11:29:06 +1000

    RMSProp code reformatting

--- src/rmsprop.h
+++ src/rmsprop.h
@@ -44,23 +44,22 @@
 
 class Rmsprop : public AbstractSgdOptimizer
 {
-public:
-    Rmsprop(
-         std::vector<double>         initial_values,
-         double                      step_size,
-         double                      decay_rate
-        
-         );
-    
-protected:
-    void do_perform_update(std::vector<double> const& gradients) override;
-    
-private:
-    double                          m_step_size;
-    double                          m_decay_rate;
-    
-   
-    std::vector<double>             m_second_moment;
+    public:
+        Rmsprop(
+                std::vector<double>         initial_values,
+                double                      step_size,
+                double                      decay_rate
+        );
+
+    protected:
+        void do_perform_update(std::vector<double> const& gradients) override;
+
+    private:
+        double                          m_step_size;
+        double                          m_decay_rate;
+
+
+        std::vector<double>             m_second_moment;
 };
 
 
